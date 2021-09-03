023352ee473d9388d97f67455fe1a5abe424afc7
ProgramadorSemx <88852393+ProgramadorSemx@users.noreply.github.com>
2021-09-03 17:59:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+function f() {
+    var h = prompt('Estamos siendo ')
+    if (h == 'We are being') {
+        alert('Perfecto')
+    } else {
+        alert('Mal')
+    }
+}
+
+function h() {
+    var f = prompt('Hemo sido')
+    if (f == 'We have been') {
+        alert('Bien')
+    } else {
+        alert('Mal')
+    }
+}
+
+function a() {
+    var f = prompt('Estabamos')
+    if (f == 'We were') {
+        alert('Bien')
+    } else {
+        alert('Mal')
+    }
+}
+
+function b() {
+    var f = prompt('Estaremos')
+    if (f == 'We will be ') {
+        alert('Bien')
+    } else {
+        alert('Mal')
+    }
+}
+
+function c() {
+    var f = prompt('Vamos a estar')
+    if (f == 'We re going to be ') {
+        alert('Bien')
+    } else {
+        alert('Mal')
+    }
+}
